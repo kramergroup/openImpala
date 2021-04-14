b4eedb8dfcc1a1bb0e9dddda932088e5aabb1d77
jameslehoux <37665786+jameslehoux@users.noreply.github.com>
2021-04-14 10:43:53 +0100

    Added amrex:finalize

--- src/io/tDatReader.cpp
+++ src/io/tDatReader.cpp
@@ -36,7 +36,7 @@ int main (int argc, char* argv[])
 {
 
   amrex::Initialize(argc, argv);
-
+  {
   // What time is it now?
   std::time_t strt_time;
   std::tm* timeinfo;
@@ -111,4 +111,8 @@ int main (int argc, char* argv[])
   // Write plot file to home dir with datetime in YYmmDDHHMM format appended
   amrex::WriteSingleLevelPlotfile(homeDir + std::string("/openimpalaresults/datreadertest") += datetime, mfv, {"phase"}, geom, 0.0, 0);
 
+  } // Ensure amrex related destructors have been called before tearing down the whole thing
+  // by putting everything in curly brackets.
+  amrex::Finalize();
+  
 }
