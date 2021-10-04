a329338f020ba45ed833d5056f1ccb88dfa6e151
jameslehoux <37665786+jameslehoux@users.noreply.github.com>
2021-10-04 11:12:20 +0100

    Tried reversing RHS

--- src/props/TortuosityHypreFill.F90
+++ src/props/TortuosityHypreFill.F90
@@ -151,11 +151,11 @@ contains
           ! perpendicular to flow direction
           if ( ( dir .eq. direction_x ) .and. (p(i-1,j,k) .ne. p(i,j,k)) .and. (i .ne. domlo(1)) ) then
             a(idx) = (/1.0,0.0,0.0,0.0,0.0,0.0,0.0/)
-            rhs(m) = vhi
+            rhs(m) = vlo
           end if
           if ( ( dir .eq. direction_x ) .and. (p(i+1,j,k) .ne. p(i,j,k)) .and. (i .ne. domhi(1)) ) then
             a(idx) = (/1.0,0.0,0.0,0.0,0.0,0.0,0.0/)
-            rhs(m) = vlo
+            rhs(m) = vhi
           end if
           if ( ( dir .eq. direction_y ) .and. (p(i,j-1,k) .ne. p(i,j,k)) .and. (j .ne. domlo(2))) then
             a(idx) = (/1.0,0.0,0.0,0.0,0.0,0.0,0.0/)
