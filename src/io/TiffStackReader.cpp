fd10875d60b1f573ccad853cd50aae1eb2551953
jameslehoux <37665786+jameslehoux@users.noreply.github.com>
2021-11-25 15:54:39 +0000

    Updated Box values

--- src/io/TiffStackReader.cpp
+++ src/io/TiffStackReader.cpp
@@ -106,7 +106,7 @@ uint32_t TiffStackReader::width()
 
 amrex::Box TiffStackReader::box()
 {
-  amrex::Box box(amrex::IntVect{0,0,0}, amrex::IntVect{m_width-1,m_height-1,m_depth+3});
+  amrex::Box box(amrex::IntVect{0,0,0}, amrex::IntVect{m_width,m_height,m_depth+2});
   return box;
 }
 
