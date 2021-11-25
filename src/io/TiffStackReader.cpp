ab45252208b831b7c5a63e30d0c39483cf269934
jameslehoux <37665786+jameslehoux@users.noreply.github.com>
2021-11-25 16:38:37 +0000

    Reverted back to working copy

--- src/io/TiffStackReader.cpp
+++ src/io/TiffStackReader.cpp
@@ -60,31 +60,10 @@ void TiffStackReader::readTiffFile()
           if (TIFFReadRGBAImageOriented(tif, m_width, m_height, raster, ORIENTATION_TOPLEFT, 0)) {
               for (long j=0; j<m_height; ++j)
               {
-                if (j==0) {
-                  for (long i=0; i<m_width; ++i)
-                 {
-                    m_raw.push_back(0);  
-                  }
-                }
-                    
                 for (long i=0; i<m_width; ++i)
                  {
-                  if (i==0) {
-                    m_raw.push_back(0);  
-                  }
-                  
-                  m_raw.push_back(-raster[i+j*m_width]-1);
-                  
-                  if (i==m_width-1) {
-                    m_raw.push_back(0);  
-                  }
+                   m_raw.push_back(-raster[i+j*m_width]-1);
                 }
-                if (j==m_height-1) {
-                  for (long i=0; i<m_width; ++i)
-                 {
-                    m_raw.push_back(0);  
-                }
-                  }
               }
           }
           _TIFFfree(raster);
@@ -127,7 +106,7 @@ uint32_t TiffStackReader::width()
 
 amrex::Box TiffStackReader::box()
 {
-  amrex::Box box(amrex::IntVect{0,0,0}, amrex::IntVect{m_width,m_height,m_depth+2});
+  amrex::Box box(amrex::IntVect{0,0,0}, amrex::IntVect{m_width-1,m_height-1,m_depth+2});
   return box;
 }
 
