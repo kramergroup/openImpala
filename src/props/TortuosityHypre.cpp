217155b917d7bac8ec156ad7f4acbad63e864a9f
jameslehoux <37665786+jameslehoux@users.noreply.github.com>
2021-11-28 13:45:50 +0000

    Rewritten periodic routine to remove typos

--- src/props/TortuosityHypre.cpp
+++ src/props/TortuosityHypre.cpp
@@ -57,15 +57,15 @@ void TortuosityHypre::setupGrids()
     
     // 3 - Set grid to be periodic
     if (m_dir==0){
-      int* periodic = [0, hi[1]-1-lo[1], hi[2]-1-lo[2]]
+      int* periodic[3] = {0, hi[1]-1-lo[1], hi[2]-1-lo[2]};
      }
     if (m_dir==1){
-      int* periodic = [hi[0]-1-lo[0], 0, hi[2]-1-lo[2]]
+      int* periodic[3] = {hi[0]-1-lo[0], 0, hi[2]-1-lo[2]};
      }
     if (m_dir==2){
-      int* periodic = [hi[0]-1-lo[0], hi[1]-1-lo[1], 0]
+      int* periodic[3] = {hi[0]-1-lo[0], hi[1]-1-lo[1], 0};
      }
-     HYPRE_StructGridSetPeriodic(m_grid, periodic) */
+     HYPRE_StructGridSetPeriodic(m_grid, periodic); 
   }
   
   // 4 - Finish setup
