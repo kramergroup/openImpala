9ec18db8ab7d27bbf615e85e36d7281c8fde533e
jameslehoux <37665786+jameslehoux@users.noreply.github.com>
2021-11-29 10:56:46 +0000

    Moved periodic routine into brackets

--- src/props/TortuosityHypre.cpp
+++ src/props/TortuosityHypre.cpp
@@ -59,12 +59,14 @@ void TortuosityHypre::setupGrids()
     // int periodic[3] = {hi[0]-1-lo[0], hi[1]-1-lo[1], hi[2]-1-lo[2]};
     /*int periodic[3] = {16, 16, 16};
     HYPRE_StructGridSetPeriodic(m_grid, periodic);*/
+    
+    int periodic[3] = {hi[0]-1-lo[0], hi[1]-1-lo[1], hi[2]-1-lo[2]};
+    HYPRE_StructGridSetPeriodic(m_grid, periodic);
       
   }
   
   // 4 - Finish setup
-  int periodic[3] = {hi[0]-1-lo[0], hi[1]-1-lo[1], hi[2]-1-lo[2]};
-  HYPRE_StructGridSetPeriodic(m_grid, periodic);
+  
   
   HYPRE_StructGridAssemble(m_grid);
   
