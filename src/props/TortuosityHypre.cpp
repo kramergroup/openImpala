566d4f9ec1bfa77c01b9828a4b6e961888af1caa
jameslehoux <37665786+jameslehoux@users.noreply.github.com>
2021-11-29 11:01:55 +0000

    Changed periodic coordinates to explicit

--- src/props/TortuosityHypre.cpp
+++ src/props/TortuosityHypre.cpp
@@ -60,7 +60,7 @@ void TortuosityHypre::setupGrids()
     /*int periodic[3] = {16, 16, 16};
     HYPRE_StructGridSetPeriodic(m_grid, periodic);*/
     
-    int periodic[3] = {hi[0]-1-lo[0], hi[1]-1-lo[1], hi[2]-1-lo[2]};
+    int periodic[3] = {33, 32, 33};
     HYPRE_StructGridSetPeriodic(m_grid, periodic);
       
   }
