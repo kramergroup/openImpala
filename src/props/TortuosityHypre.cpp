b8903d567caa31242d7d32b29320d98163ee60a4
jameslehoux <37665786+jameslehoux@users.noreply.github.com>
2021-12-02 11:25:32 +0000

    Tried moving periodic outside of mpi brackets

--- src/props/TortuosityHypre.cpp
+++ src/props/TortuosityHypre.cpp
@@ -58,14 +58,18 @@ void TortuosityHypre::setupGrids()
     // 3 - Set grid to be periodic
     /*int periodic[3] = {hi[0]-lo[0], hi[1]-lo[1], hi[2]-lo[2]};
     HYPRE_StructGridSetPeriodic(m_grid, periodic);*/
-    amrex::Print() << "X high: " << hi[0] << std::endl
+      
+  }
+  
+  const amrex::Box domain = m_geom.Domain();
+  auto lo = TortuosityHypre::loV(domain);
+  auto hi = TortuosityHypre::hiV(domain);
+      amrex::Print() << "X high: " << hi[0] << std::endl
                    << "X low: " << lo[0] << std::endl
                    << "Y high: " << hi[1] << std::endl
                    << "Y low: " << lo[1] << std::endl
                    << "Z high: " << hi[2] << std::endl
                    << "Z low: " << lo[2] << std::endl;
-      
-  }
   
   // 4 - Finish setup 
   HYPRE_StructGridAssemble(m_grid);
