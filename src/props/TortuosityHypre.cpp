21594570c459de86e9add251051f9d22bddfd065
jameslehoux <37665786+jameslehoux@users.noreply.github.com>
2021-12-03 16:43:08 +0000

    Tried new periodic coordinates order

--- src/props/TortuosityHypre.cpp
+++ src/props/TortuosityHypre.cpp
@@ -59,7 +59,7 @@ void TortuosityHypre::setupGrids()
     const amrex::Box domain = m_geom.Domain();
     auto domainlo = TortuosityHypre::loV(domain);
     auto domainhi = TortuosityHypre::hiV(domain);
-    int periodic[3] = {domainhi[1]-domainlo[1], domainhi[2]-domainlo[2], domainhi[0]-domainlo[0]};
+    int periodic[3] = {domainhi[2]-domainlo[2], domainhi[0]-domainlo[0], domainhi[1]-domainlo[1]};
     HYPRE_StructGridSetPeriodic(m_grid, periodic);
     
   }
