008a14892874dd1610830516bbf7d63efed893dd
jameslehoux <37665786+jameslehoux@users.noreply.github.com>
2021-12-07 10:29:47 +0000

    Changed to Jacobian solver for all directions

--- src/props/Diffusion.cpp
+++ src/props/Diffusion.cpp
@@ -231,7 +231,7 @@ int main (int argc, char* argv[])
   amrex::Print() << std::endl << " Direction: X" << std::endl;
 
   // Compute tortuosity in x direction
-  TortuosityHypre tortuosityx(geom,ba,dm,mf_phase,vf.value(),1,Direction::X,TortuosityHypre::SolverType::FlexGMRES, RESULTS_PATH);
+  TortuosityHypre tortuosityx(geom,ba,dm,mf_phase,vf.value(),1,Direction::X,TortuosityHypre::SolverType::Jacobi, RESULTS_PATH);
 
   amrex::Real tau_value_x = tortuosityx.value();
   amrex::Print() << " Tortuosity value: " << tau_value_x << std::endl;
@@ -253,7 +253,7 @@ int main (int argc, char* argv[])
   amrex::Print() << std::endl << " Direction: Z" << std::endl;
 
   // Compute tortuosity in z direction
-  TortuosityHypre tortuosityz(geom,ba,dm,mf_phase,vf.value(),1,Direction::Z,TortuosityHypre::SolverType::FlexGMRES, RESULTS_PATH);
+  TortuosityHypre tortuosityz(geom,ba,dm,mf_phase,vf.value(),1,Direction::Z,TortuosityHypre::SolverType::Jacobi, RESULTS_PATH);
 
   amrex::Real tau_value_z = tortuosityz.value();
   amrex::Print() << " Tortuosity value: " << tau_value_z << std::endl;
