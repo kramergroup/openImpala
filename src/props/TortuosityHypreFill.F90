419be7e90d1c48ed2f2650ebb3acb16a7a4efb5b
jameslehoux <37665786+jameslehoux@users.noreply.github.com>
2022-05-15 23:34:25 +0100

    Updated with box limits

--- src/props/TortuosityHypreFill.F90
+++ src/props/TortuosityHypreFill.F90
@@ -150,27 +150,27 @@ contains
           ! Change to Neumann condition at domain boundaries
           ! perpendicular to flow direction
          if ( p(i,j,k) .eq. phase) then
-          if ( ( dir .eq. direction_x ) .and. (p(i-1,j,k) .ne. p(i,j,k)) .and. (i .ne. domlo(1)) .and. (i .ne. domhi(1))  ) then
+          if ( ( dir .eq. direction_x ) .and. (p(i-1,j,k) .ne. p(i,j,k)) .and. (i .ne. bxlo(1)) .and. (i .ne. bxhi(1))  ) then
             a(idx) = (/1.0,0.0,0.0,0.0,0.0,0.0,0.0/)
             rhs(m) = vhi
           end if
-          if ( ( dir .eq. direction_x ) .and. (p(i+1,j,k) .ne. p(i,j,k)) .and. (i .ne. domhi(1)) .and. (i .ne. domlo(1))  ) then
+          if ( ( dir .eq. direction_x ) .and. (p(i+1,j,k) .ne. p(i,j,k)) .and. (i .ne. bxhi(1)) .and. (i .ne. bxlo(1))  ) then
             a(idx) = (/1.0,0.0,0.0,0.0,0.0,0.0,0.0/)
             rhs(m) = vlo
           end if
-          if ( ( dir .eq. direction_y ) .and. (p(i,j-1,k) .ne. p(i,j,k)) .and. (j .ne. domlo(2)) .and. (j .ne. domhi(2))  ) then
+          if ( ( dir .eq. direction_y ) .and. (p(i,j-1,k) .ne. p(i,j,k)) .and. (j .ne. bxlo(2)) .and. (j .ne. bxhi(2))  ) then
             a(idx) = (/1.0,0.0,0.0,0.0,0.0,0.0,0.0/)
             rhs(m) = vhi
           end if
-          if ( ( dir .eq. direction_y ) .and. (p(i,j+1,k) .ne. p(i,j,k)) .and. (j .ne. domhi(2)) .and. (j .ne. domlo(2)) ) then
+          if ( ( dir .eq. direction_y ) .and. (p(i,j+1,k) .ne. p(i,j,k)) .and. (j .ne. bxhi(2)) .and. (j .ne. bxlo(2)) ) then
             a(idx) = (/1.0,0.0,0.0,0.0,0.0,0.0,0.0/)
             rhs(m) = vlo
           end if
-          if ( ( dir .eq. direction_z ) .and. (p(i,j,k-1) .ne. p(i,j,k)) .and. (k .ne. domlo(3)) .and. (k .ne. domhi(3)) ) then
+          if ( ( dir .eq. direction_z ) .and. (p(i,j,k-1) .ne. p(i,j,k)) .and. (k .ne. bxlo(3)) .and. (k .ne. bxhi(3)) ) then
             a(idx) = (/1.0,0.0,0.0,0.0,0.0,0.0,0.0/)
             rhs(m) = vhi
           end if
-          if ( ( dir .eq. direction_z ) .and. (p(i,j,k+1) .ne. p(i,j,k)) .and. (k .ne. domhi(3)) .and. (k .ne. domlo(3)) ) then
+          if ( ( dir .eq. direction_z ) .and. (p(i,j,k+1) .ne. p(i,j,k)) .and. (k .ne. bxhi(3)) .and. (k .ne. bxlo(3)) ) then
             a(idx) = (/1.0,0.0,0.0,0.0,0.0,0.0,0.0/)
             rhs(m) = vlo
           end if
