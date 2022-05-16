d228799e2013c50543b411996ba39166099be656
jameslehoux <37665786+jameslehoux@users.noreply.github.com>
2022-05-16 10:28:47 +0100

    Updated boundary conditions to reflect ghost cell layer

--- src/props/TortuosityHypreFill.F90
+++ src/props/TortuosityHypreFill.F90
@@ -123,40 +123,46 @@ contains
             rhs(m) = 0.0
             
             ! Change to one-sided Dirichlet condition at phase boundaries
-            if ( (p(i-1,j,k) .ne. p(i,j,k)) .and. (i .ne. domlo(1)) .and. (i .ne. domhi(1)) )  then
+            if ( (p(i-1,j,k) .ne. p(i,j,k)) .and. (i .ne. domlo(1)) .and. (i .ne. domhi(1)) &
+            .and. (i .ne. domlo(1)+1) .and. (i .ne. domhi(1)-1))  then
               a(idx) = a(idx) + (/-1.0,1.0,0.0,0.0,0.0,0.0,0.0/)
             end if
-            if ( (p(i+1,j,k) .ne. p(i,j,k)) .and. (i .ne. domhi(1)) .and. (i .ne. domlo(1)) ) then
+            if ( (p(i+1,j,k) .ne. p(i,j,k)) .and. (i .ne. domhi(1)) .and. (i .ne. domlo(1)) &
+            .and. (i .ne. domlo(1)+1) .and. (i .ne. domhi(1)-1)) then
               a(idx) = a(idx) + (/-1.0,0.0,1.0,0.0,0.0,0.0,0.0/)
             end if
-            if ( (p(i,j-1,k) .ne. p(i,j,k)) .and. (j .ne. domlo(2)) .and. (j .ne. domhi(2)) ) then
+            if ( (p(i,j-1,k) .ne. p(i,j,k)) .and. (j .ne. domlo(2)) .and. (j .ne. domhi(2)) &
+            .and. (j .ne. domlo(2)+1) .and. (j .ne. domhi(2)-1) ) then
               a(idx) = a(idx) + (/-1.0,0.0,0.0,1.0,0.0,0.0,0.0/)
             end if
-            if ( (p(i,j+1,k) .ne. p(i,j,k)) .and. (j .ne. domhi(2)) .and. (j .ne. domlo(2)) ) then
+            if ( (p(i,j+1,k) .ne. p(i,j,k)) .and. (j .ne. domhi(2)) .and. (j .ne. domlo(2)) &
+            .and. (j .ne. domlo(2)+1) .and. (j .ne. domhi(2)-1) ) then
               a(idx) = a(idx) + (/-1.0,0.0,0.0,0.0,1.0,0.0,0.0/)
             end if
-            if ( (p(i,j,k-1) .ne. p(i,j,k)) .and. (k .ne. domlo(3)) .and. (k .ne. domhi(3)) ) then
+            if ( (p(i,j,k-1) .ne. p(i,j,k)) .and. (k .ne. domlo(3)) .and. (k .ne. domhi(3)) &
+            .and. (k .ne. domlo(3)+1) .and. (k .ne. domhi(3)-1)) then
               a(idx) = a(idx) + (/-1.0,0.0,0.0,0.0,0.0,1.0,0.0/)
             end if 
-            if ( (p(i,j,k+1) .ne. p(i,j,k)) .and. (k .ne. domhi(3)) .and. (k .ne. domlo(3)) ) then
+            if ( (p(i,j,k+1) .ne. p(i,j,k)) .and. (k .ne. domhi(3)) .and. (k .ne. domlo(3)) &
+            .and. (k .ne. domhi(3)-1) .and. (k .ne. domlo(3)+1)) then
               a(idx) = a(idx) + (/-1.0,0.0,0.0,0.0,0.0,0.0,1.0/)
             end if
-            if ( (i .eq. domlo(1)) .and. (p(i,j,k) .ne. p(domhi(1),j,k))  )  then
+            if ( (i .eq. domlo(1)+1) .and. (p(i,j,k) .ne. p(domhi(1)-1,j,k))  )  then
               a(idx) = a(idx) + (/-1.0,1.0,0.0,0.0,0.0,0.0,0.0/)
             end if
-            if ( (i .eq. domhi(1)) .and. (p(i,j,k) .ne. p(domlo(1),j,k))  )  then
+            if ( (i .eq. domhi(1)-1) .and. (p(i,j,k) .ne. p(domlo(1)+1,j,k))  )  then
               a(idx) = a(idx) + (/-1.0,0.0,1.0,0.0,0.0,0.0,0.0/)
             end if
-            if ( (j .eq. domlo(2)) .and. (p(i,j,k) .ne. p(i,domhi(2),k))  )  then
+            if ( (j .eq. domlo(2)+1) .and. (p(i,j,k) .ne. p(i,domhi(2)-1,k))  )  then
               a(idx) = a(idx) + (/-1.0,0.0,0.0,1.0,0.0,0.0,0.0/)
             end if
-            if ( (j .eq. domhi(2)) .and. (p(i,j,k) .ne. p(i,domlo(2),k))  )  then
+            if ( (j .eq. domhi(2)-1) .and. (p(i,j,k) .ne. p(i,domlo(2)+1,k))  )  then
               a(idx) = a(idx) + (/-1.0,0.0,0.0,0.0,1.0,0.0,0.0/)
             end if
-            if ( (k .eq. domlo(3)) .and. (p(i,j,k) .ne. p(i,j,domhi(3)))  )  then
+            if ( (k .eq. domlo(3)+1) .and. (p(i,j,k) .ne. p(i,j,domhi(3)-1))  )  then
               a(idx) = a(idx) + (/-1.0,0.0,0.0,0.0,0.0,1.0,0.0/)
             end if
-            if ( (k .eq. domhi(3)) .and. (p(i,j,k) .ne. p(i,j,domlo(3)))  )  then
+            if ( (k .eq. domhi(3)-1) .and. (p(i,j,k) .ne. p(i,j,domlo(3)+1))  )  then
               a(idx) = a(idx) + (/-1.0,0.0,0.0,0.0,0.0,0.0,1.0/)
             end if
             
@@ -167,55 +173,61 @@ contains
           ! Change to Neumann condition at domain boundaries
           ! perpendicular to flow direction
          if ( p(i,j,k) .eq. phase) then
-          if ( ( dir .eq. direction_x ) .and. (p(i-1,j,k) .ne. p(i,j,k)) .and. (i .ne. domlo(1)) .and. (i .ne. domhi(1)) ) then
+          if ( ( dir .eq. direction_x ) .and. (p(i-1,j,k) .ne. p(i,j,k)) .and. (i .ne. domlo(1)) .and. (i .ne. domhi(1)) &
+            .and. (i .ne. domlo(1)+1) .and. (i .ne. domhi(1)-1)) then
             a(idx) = (/1.0,0.0,0.0,0.0,0.0,0.0,0.0/)
             rhs(m) = vhi
           end if
-          if ( ( dir .eq. direction_x ) .and. (p(i+1,j,k) .ne. p(i,j,k)) .and. (i .ne. domhi(1)) .and. (i .ne. domlo(1)) ) then
+          if ( ( dir .eq. direction_x ) .and. (p(i+1,j,k) .ne. p(i,j,k)) .and. (i .ne. domhi(1)) .and. (i .ne. domlo(1)) &
+            .and. (i .ne. domlo(1)+1) .and. (i .ne. domhi(1)-1)) then
             a(idx) = (/1.0,0.0,0.0,0.0,0.0,0.0,0.0/)
             rhs(m) = vlo
           end if
-          if ( ( dir .eq. direction_y ) .and. (p(i,j-1,k) .ne. p(i,j,k)) .and. (j .ne. domlo(2)) .and. (j .ne. domhi(2)) ) then
+          if ( ( dir .eq. direction_y ) .and. (p(i,j-1,k) .ne. p(i,j,k)) .and. (j .ne. domlo(2)) .and. (j .ne. domhi(2)) &
+            .and. (j .ne. domlo(2)+1) .and. (j .ne. domhi(2)-1) ) then
             a(idx) = (/1.0,0.0,0.0,0.0,0.0,0.0,0.0/)
             rhs(m) = vhi
           end if
-          if ( ( dir .eq. direction_y ) .and. (p(i,j+1,k) .ne. p(i,j,k)) .and. (j .ne. domhi(2)) .and. (j .ne. domlo(2))  ) then
+          if ( ( dir .eq. direction_y ) .and. (p(i,j+1,k) .ne. p(i,j,k)) .and. (j .ne. domhi(2)) .and. (j .ne. domlo(2)) &
+            .and. (j .ne. domlo(2)+1) .and. (j .ne. domhi(2)-1) ) then
             a(idx) = (/1.0,0.0,0.0,0.0,0.0,0.0,0.0/)
             rhs(m) = vlo
           end if
-          if ( ( dir .eq. direction_z ) .and. (p(i,j,k-1) .ne. p(i,j,k)) .and. (k .ne. domlo(3)) .and. (k .ne. domhi(3)) ) then
+          if ( ( dir .eq. direction_z ) .and. (p(i,j,k-1) .ne. p(i,j,k)) .and. (k .ne. domlo(3)) .and. (k .ne. domhi(3)) &
+            .and. (k .ne. domhi(3)-1) .and. (k .ne. domlo(3)+1)) then
             a(idx) = (/1.0,0.0,0.0,0.0,0.0,0.0,0.0/)
             rhs(m) = vhi
           end if
-          if ( ( dir .eq. direction_z ) .and. (p(i,j,k+1) .ne. p(i,j,k)) .and. (k .ne. domhi(3)) .and. (k .ne. domlo(3)) ) then
+          if ( ( dir .eq. direction_z ) .and. (p(i,j,k+1) .ne. p(i,j,k)) .and. (k .ne. domhi(3)) .and. (k .ne. domlo(3))  &
+            .and. (k .ne. domhi(3)-1) .and. (k .ne. domlo(3)+1)) then
             a(idx) = (/1.0,0.0,0.0,0.0,0.0,0.0,0.0/)
             rhs(m) = vlo
           end if
           
-!          if ( ( dir .eq. direction_x ) .and. (i .eq. domlo(1)) .and. (p(i,j,k) .ne. p(domhi(1),j,k))  )  then
-!            a(idx) = (/1.0,0.0,0.0,0.0,0.0,0.0,0.0/)
-!            rhs(m) = vhi
-!          end if
-!          if ( ( dir .eq. direction_x ) .and. (i .eq. domhi(1)) .and. (p(i,j,k) .ne. p(domlo(1),j,k))  )  then
-!            a(idx) = (/1.0,0.0,0.0,0.0,0.0,0.0,0.0/)
-!            rhs(m) = vlo
-!          end if
- !         if ( ( dir .eq. direction_y ) .and. (j .eq. domlo(2)) .and. (p(i,j,k) .ne. p(i,domhi(2),k))  )  then
-  !          a(idx) = (/1.0,0.0,0.0,0.0,0.0,0.0,0.0/)
-   !         rhs(m) = vhi
-    !      end if
-     !     if ( ( dir .eq. direction_y ) .and. (j .eq. domhi(2)) .and. (p(i,j,k) .ne. p(i,domlo(2),k))  )  then
-      !      a(idx) = (/1.0,0.0,0.0,0.0,0.0,0.0,0.0/)
-       !     rhs(m) = vlo
-        !  end if
-         ! if ( ( dir .eq. direction_z ) .and. (k .eq. domlo(3)) .and. (p(i,j,k) .ne. p(i,j,domhi(3)))  )  then
-          !  a(idx) = (/1.0,0.0,0.0,0.0,0.0,0.0,0.0/)
-           ! rhs(m) = vhi
-!          end if
- !         if ( ( dir .eq. direction_z ) .and. (k .eq. domhi(3)) .and. (p(i,j,k) .ne. p(i,j,domlo(3)))  )  then
-  !          a(idx) = (/1.0,0.0,0.0,0.0,0.0,0.0,0.0/)
-   !         rhs(m) = vlo
-    !      end if
+          if ( ( dir .eq. direction_x ) .and. (i .eq. domlo(1)+1) .and. (p(i,j,k) .ne. p(domhi(1)-1,j,k))  )  then
+            a(idx) = (/1.0,0.0,0.0,0.0,0.0,0.0,0.0/)
+            rhs(m) = vhi
+          end if
+          if ( ( dir .eq. direction_x ) .and. (i .eq. domhi(1)-1) .and. (p(i,j,k) .ne. p(domlo(1)+1,j,k))  )  then
+            a(idx) = (/1.0,0.0,0.0,0.0,0.0,0.0,0.0/)
+            rhs(m) = vlo
+          end if
+          if ( ( dir .eq. direction_y ) .and. (j .eq. domlo(2)+1) .and. (p(i,j,k) .ne. p(i,domhi(2)-1,k))  )  then
+            a(idx) = (/1.0,0.0,0.0,0.0,0.0,0.0,0.0/)
+            rhs(m) = vhi
+          end if
+          if ( ( dir .eq. direction_y ) .and. (j .eq. domhi(2)-1) .and. (p(i,j,k) .ne. p(i,domlo(2)+1,k))  )  then
+            a(idx) = (/1.0,0.0,0.0,0.0,0.0,0.0,0.0/)
+            rhs(m) = vlo
+          end if
+          if ( ( dir .eq. direction_z ) .and. (k .eq. domlo(3)+1) .and. (p(i,j,k) .ne. p(i,j,domhi(3)-1))  )  then
+            a(idx) = (/1.0,0.0,0.0,0.0,0.0,0.0,0.0/)
+            rhs(m) = vhi
+          end if
+          if ( ( dir .eq. direction_z ) .and. (k .eq. domhi(3)-1) .and. (p(i,j,k) .ne. p(i,j,domlo(3)+1))  )  then
+            a(idx) = (/1.0,0.0,0.0,0.0,0.0,0.0,0.0/)
+            rhs(m) = vlo
+          end if
           
         end if
           
