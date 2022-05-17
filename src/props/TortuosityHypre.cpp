ae4df0bc518fd2982ccd7b725f1f70fc4f43ba1e
jameslehoux <37665786+jameslehoux@users.noreply.github.com>
2022-05-17 16:47:03 +0100

    Added periodic routine within brackets

--- src/props/TortuosityHypre.cpp
+++ src/props/TortuosityHypre.cpp
@@ -45,17 +45,6 @@ void TortuosityHypre::setupGrids()
   // 1 - Initialise the grid owned by this MPI rank
   HYPRE_StructGridCreate(MPI_COMM_WORLD, AMREX_SPACEDIM, &m_grid);
          // 3 - Set grid to be periodic
-  
-  const amrex::Box domain = m_geom.Domain();
-  auto domainlo = TortuosityHypre::loV(domain);
-  auto domainhi = TortuosityHypre::hiV(domain);    
-  int periodic[3] = {domainhi[0]-domainlo[0], domainhi[1]-domainlo[1], domainhi[2]-domainlo[2]};
-    
-  amrex::Print() << std::endl << " Domain low: "
-                    << domainlo << std::endl << " Domain high: "
-                    << domainhi << std::endl;  
-  
-  HYPRE_StructGridSetPeriodic(m_grid, periodic);
  
 
   // 2 - Configure the dimensions of each box owned by this MPI rank
@@ -68,10 +57,22 @@ void TortuosityHypre::setupGrids()
 
     HYPRE_StructGridSetExtents(m_grid, lo.data(), hi.data());
     
+    const amrex::Box domain = m_geom.Domain();
+    auto domainlo = TortuosityHypre::loV(domain);
+    auto domainhi = TortuosityHypre::hiV(domain);    
+    int periodic[3] = {domainhi[0]-domainlo[0], domainhi[1]-domainlo[1], domainhi[2]-domainlo[2]};
+    
+    amrex::Print() << std::endl << " Domain low: "
+                    << domainlo << std::endl << " Domain high: "
+                    << domainhi << std::endl;  
+  
+    HYPRE_StructGridSetPeriodic(m_grid, periodic);
+ 
     /*
     int periodic[3] = {hi[0]-lo[0], hi[1]-lo[1], hi[2]-lo[2]};
 
     HYPRE_StructGridSetPeriodic(m_grid, periodic);
+    
     */
    /*     // 3 - Set grid to be periodic
   const amrex::Box domain = m_geom.Domain();
