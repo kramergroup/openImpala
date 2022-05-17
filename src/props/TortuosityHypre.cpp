53b40ca2752d4f2ab970c3b5657c4802a92ef8cd
jameslehoux <37665786+jameslehoux@users.noreply.github.com>
2022-05-17 13:39:19 +0100

    Updated with additional ghost cell layer

--- src/props/TortuosityHypre.cpp
+++ src/props/TortuosityHypre.cpp
@@ -84,7 +84,8 @@ void TortuosityHypre::setupGrids()
   amrex::Print() << std::endl << " Domain low: "
                     << domainlo << std::endl << " Domain high: "
                     << domainhi << std::endl;  
-                    
+  
+  HYPRE_StructGridSetNumGhost(m_grid, 1)
   HYPRE_StructGridSetPeriodic(m_grid, periodic);
 
   // 4 - Finish setup 
