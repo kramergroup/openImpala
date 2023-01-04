71699e217c5bbfc647ed159cc5b470fb85a40947
Denis Kramer <d.kramer@hsu-hh.de>
2023-01-04 19:06:22 +0100

    some cleaning

--- src/python/pyoimp.cpp
+++ src/python/pyoimp.cpp
@@ -29,23 +29,27 @@ void finalize(amrex::AMReX* amrex = nullptr)
   
 }
 
-bool isInitialized()
-{
-  return amrex::Initialized();
-}
-
 BOOST_PYTHON_FUNCTION_OVERLOADS(finalize_overloads,finalize,0,1)
 
 BOOST_PYTHON_MODULE(pyoimp)
 {
+  
   using namespace boost::python;
+
+  /* Register top-level enviroment classes */
 
   // Enable passing of smart pointer to amrex
   class_<amrex::AMReX>("AMReX");
-  
-  // We return an unmanaged raw pointer, because the lifetime of amrex is managed 
-  // by explicit calls to initialize/finalize
+  register_ptr_to_python<boost::shared_ptr<amrex::AMReX>>();
+
+
+  /* AMReX environment management */
+
+  // We return an unmanaged raw pointer from initialize, because the lifetime of amrex is managed 
+  // by explicit calls to initialize/finalize. Automatically destructing/deleting amrex pointers
+  // leads to segmentation faults.
   def("initialize", initialize, return_value_policy<reference_existing_object>(), "Initialize the environment");
   def("finalize", finalize, "Clean-up environment");
-  def("isInitialized", isInitialized, "Returns true if the enviroment is initialized");
+  def("isInitialized", amrex::Initialized, "Returns true if the enviroment is initialized");
+
 }
